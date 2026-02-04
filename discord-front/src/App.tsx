a75431390adfe134e76cdbcd7161db01f52bf236
Melissa Ferrari <melissa.ferrari.v@gmail.com>
2026-02-03 21:59:53 -0300

    se modificaron archivos de formato ts a tsx

---
+++ discord-front/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  console.log('App renderizando')
+  return (
+    <div style={{ background: 'red', color: 'white', padding: '20px' }}>
+      HOLA REACT
+    </div>
+  )
+}
+
+export default App
